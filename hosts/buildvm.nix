5ac712dc23bb391d5a23819c60788f1f2cb78189
Alex Aridgides <alexanderaridgides@gmail.com>
2025-07-04 19:59:56 -0700

    add password-less sudo for VM

--- hosts/buildvm.nix
+++ hosts/buildvm.nix
@@ -5,4 +5,14 @@
   services.xserver.videoDrivers = [ "modesetting" ];
   
   services.qemuGuest.enable = true;
+  
+  # VM-specific user configuration
+  users.users.a = {
+    password = "buildvm";
+    # Enable password-less sudo for VM convenience
+    extraGroups = [ "wheel" ];
+  };
+  
+  # Allow wheel group to use sudo without password in VM
+  security.sudo.wheelNeedsPassword = false;
 }
